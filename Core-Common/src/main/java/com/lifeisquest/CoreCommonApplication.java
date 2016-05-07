2b8a14070a95b1c09658d16b7bf2db92f94ae010
Jason Park <jasondevelop@outlook.com>
2016-05-07 22:34:42 +0900

    메이븐 빌드가 실패하는 문제 해결.

---
+++ Core-Common/src/main/java/com/lifeisquest/CoreCommonApplication.java
@@ -0,0 +1,16 @@
+package com.lifeisquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jason Park
+ * @since v1.0.0
+ */
+@SpringBootApplication
+public class CoreCommonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CoreCommonApplication.class, args);
+  }
+}
