0c1f3a1f46ca305d0ebd798000ba4cfb7e6f0dd8
Jason Park <jasondevelop@outlook.com>
2016-05-12 20:39:36 +0900

    #1 | CSRF 방어 코드 추가.

--- Web-Test/src/main/java/com/lifeisquest/config/SecurityConfig.java
+++ Web-Test/src/main/java/com/lifeisquest/config/SecurityConfig.java
@@ -6,9 +6,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.WebUtils;
+
+import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -34,6 +37,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
           .permitAll()
         .anyRequest()
           .authenticated()
-        .and().logout().logoutSuccessUrl("/").permitAll();
+        .and().logout().logoutSuccessUrl("/").permitAll()
+        .and().csrf().csrfTokenRepository(csrfTokenRepository())
+        .and().addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+  }
+
+  private CsrfTokenRepository csrfTokenRepository() {
+    HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+    repository.setHeaderName("X-XSRF-TOKEN");
+    return repository;
+  }
+
+  private Filter csrfHeaderFilter() {
+    return new OncePerRequestFilter() {
+      @Override
+      protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                            FilterChain filterChain) throws ServletException, IOException {
+        CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        if (csrf != null) {
+          Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+          String token = csrf.getToken();
+          if (cookie == null || token != null && !token.equals(cookie.getValue())) {
+            cookie = new Cookie("XSRF-TOKEN", token);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+          }
+        }
+        filterChain.doFilter(request, response);
+      }
+    };
   }
 }
