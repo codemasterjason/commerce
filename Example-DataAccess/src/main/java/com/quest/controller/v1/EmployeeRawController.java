c263fc40475a0bd24c121ffec75f463400a291a1
Jason Park <jasondevelop@outlook.com>
2016-05-22 23:41:21 +0900

    #2 | request object에 validation 추가. pagination 추가.

--- Example-DataAccess/src/main/java/com/quest/controller/v1/EmployeeRawController.java
+++ Example-DataAccess/src/main/java/com/quest/controller/v1/EmployeeRawController.java
@@ -1,10 +1,11 @@
-package com.quest.controller;
+package com.quest.controller.v1;
 
 import com.google.common.collect.Lists;
 
 import com.lifeisquest.annotation.Get;
 import com.lifeisquest.util.MessageCode;
 import com.lifeisquest.util.ResponseObj;
+import com.quest.model.RowBounds;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,14 +16,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 import javax.persistence.Query;
+import javax.validation.Valid;
 
 /**
  * @author Jason Park
  * @since v1.0.0
  */
 @RestController
-@RequestMapping("row_level/v1")
-public class RowLevelController {
+@RequestMapping("v1/employee")
+public class EmployeeRawController {
 
   private EntityManagerFactory emf;
 
@@ -36,18 +38,22 @@ public class RowLevelController {
    *
    * @return ResponseObj
    */
-  @Get("/employee")
-  public ResponseObj getEmployee() {
+  @Get("")
+  public ResponseObj getEmployee(@Valid RowBounds rowBounds) {
     EntityManager em = this.emf.createEntityManager();
     List<Object> resultList = Lists.newArrayList();
     try {
-      Query query = em.createQuery("from Employees as E").setMaxResults(2);
+      Query query = em.createQuery("from Employees as E")
+          .setFirstResult(rowBounds.getOffset() * rowBounds.getRowCount())
+          .setMaxResults(rowBounds.getRowCount());
       resultList = query.getResultList();
     } finally {
       if (em != null) {
         em.close();
       }
     }
+
+    System.out.println(rowBounds);
 
     return new ResponseObj(MessageCode.SUCCESS, resultList);
   }
